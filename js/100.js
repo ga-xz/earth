d10c79f6c7e2729529d9d41e2549eb8a134eb0d8
ga-xz <149323120+ga-xz@users.noreply.github.com>
2023-11-25 22:50:18 +0800

    Add files via upload

--- js/100.js
+++ js/100.js
@@ -1,8 +1,8 @@
-$ (function(){
+/*$ (function(){
 var text = document.getElementById('text');
 var confirm = document.querySelector('#confirm');
 
-var times = 3;
+var times = null;
 var mydata = localStorage.getItem("mydata");
 if (mydata == null && $('#fulsh').is(':hidden')) {
   setTimeout(function(){
@@ -31,23 +31,116 @@ confirm.addEventListener('click', function (){
      },3000);
         
   } else  if (hex_md5(text.value) == pas){
-    setTimeout(function (){
-      $('#fulsh').fadeOut(210);
-    },500);
+    $('#fulsh').fadeOut(200);
     localStorage.setItem("mydata", times);
-    var din = new Audio('mp3/dind.mp3');
-    din.play();
     //alert('hbv')
   } else {
      $('#blps').fadeIn(210);
-     var outw = new Audio('mp3/out.mp3');
+     var outw = new Audio('mp3/ou.mp3');
     outw.play();
-  
      setTimeout(function (){
        $('#blps').fadeOut(210);
      },3000);
-   }
-  });
+  }
+
+  })
   // 清除所有的缓存数据
 //localStorage.clear();
-})
+});*/
+$(function(){
+  //弹窗提示
+  $('#full-con').click(function(){
+    setTimeout(function(){
+      $('#full').fadeOut(400);
+    },300);
+  });
+  // 从上往下加载出来
+    //$(".hb").slideDown(1200);
+    //显示收款码
+    $('#sup-ok').click(function(){
+      $('.yard').fadeIn(400);
+    });
+  //关闭收款码
+  $('#shut').click(function(){
+    $('.yard').fadeOut(400);
+  });
+  //红包
+  $('#set-hb').click(function(){
+    $('#hb-bok').slideDown(300);
+  });
+  $('#hb-hp').click(function(){
+    $('#hb-bok').slideUp(300);
+  });
+  //微信切换
+  $('#set-wx').click(function(){
+    $('.s-wx').fadeIn(400);
+    $('.s-zfb').fadeOut(400);
+    $('.s-qqzf').fadeOut(400);
+  });
+//支付宝切换
+ $('#set-zfb').click(function(){
+    $('.s-zfb').fadeIn(400);
+    $('.s-wx').fadeOut(400);
+    $('.s-qqzf').fadeOut(400);
+  });
+  //QQ切换
+  $('#set-qq').click(function(){
+    $('.s-qqzf').fadeIn(400);
+    $('.s-zfb').fadeOut(400);
+    $('.s-wx').fadeOut(400);
+  });
+  // 从下往上隐藏
+  setTimeout(function (){
+    //$(".hb").slideUp(1200);
+    //$('.hb').fadeOut(300);
+  },2000);
+  var seq = document.querySelector('.set-q');
+  var i = 0;
+  $('#set').click(function (){
+    i++;
+    var int = document.getElementById('set-int');
+    if(i % 2 == 1){
+    int.style.transform = 'translateX(-240px)';
+  setTimeout( function(){
+    seq.src = 'img/Rwi.svg';
+   },100);
+  } else {
+    int.style.transform = 'translateX(240px)';
+  setTimeout( function(){
+    seq.src = 'img/RLe.svg';
+    },150);
+  }
+ });
+ $('#sthu').click(function(){
+   var int = document.getElementById('set-int');
+   int.style.transform = 'translateX(240px)';
+   setTimeout( function(){
+     seq.src = 'img/RLe.svg';
+   },100);
+   i--;
+ });
+ //测绘法
+ var mapin = document.querySelector('#maping');
+ var main = document.querySelector('#mapin');
+ $(mapin).click(function(){
+   main.style.right = '0%';
+ });
+ var mapou = document.getElementById('mapin-ou');
+ $(mapou).click(function(){
+   main.style.right = '-95%';
+ });
+ //个人
+ $('#human').click(function (){
+   setTimeout(function(){
+     $('#full').fadeIn(400);
+   },300)
+ });
+ //版本
+ var edion = document.getElementById('edion');
+ edion.onclick = function(){
+   $('#edito').slideDown(600);
+ };
+ $('#edi-ou').click(function (){
+   $('#edito').slideUp(600);
+ });
+});
--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 var viewer = new Cesium.Viewer('cesiumContainer', {
-    //添加地形
     terrain: Cesium.Terrain.fromWorldTerrain({
-        // requestWaterMask: true,
-        // requestVertexNormals: true
+         //AbortControllerrequestWaterMask: true,
+         //requestVertexNormals: true
     }),
+   
     navigationHelpButton: false, // 隐藏帮助按钮
     fullscreenButton: false, // 隐藏全屏按钮
     homeButton: false, // 隐藏主页按钮
@@ -12,13 +12,12 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
     baseLayerPicker: false, // 隐藏底图选择器
     timeline: false, // 隐藏时间轴
     animation: false, // 隐藏动画控件
-    //skyAtmosphere: false, //隐藏大气层
+    skyAtmosphere: false, //隐藏大气层
     shouldAnimate: true, // 允许动画播放
     infoBox:false, // 禁用信息框
     //useDefaultRenderLoop: false, // 关闭默认渲染循环，以便手动控制加载
     //shouldBatchLoad: true, // 开启批量加载
-    
-});
+ });
 // 抗锯齿
 viewer.scene.postProcessStages.fxaa.enabled = true;
 // 优化性能
@@ -30,9 +29,9 @@ viewer.scene.screenSpaceCameraController.constrainedPitch = Cesium.Math.toRadian
 // 隐藏底部文字和logo
 viewer.cesiumWidget.creditContainer.style.display = "none";
 // 限制地球放大范围
-viewer.scene.screenSpaceCameraController.minimumZoomDistance = 25;
+viewer.scene.screenSpaceCameraController.minimumZoomDistance = 100;
 // 限制地球缩小范围
-viewer.scene.screenSpaceCameraController.maximumZoomDistance = 22000000;
+viewer.scene.screenSpaceCameraController.maximumZoomDistance = 23500000;
 // 移除当前图层
 viewer.imageryLayers.removeAll();
 // 隐藏默认的太阳渲染
@@ -45,10 +44,14 @@ viewer.scene.skyBox.show = false;
 // 自定义一个图层
 var gog = new Cesium.UrlTemplateImageryProvider({
     //url: 'https://gac-geo.googlecnapps.cn/maps/vt?lyrs=s,%40781=&x={x}&y={y}&z={z}&src=app&scale=2&from=app'
-    url: 'https://gac-geo.googlecnapps.cn/maps/vt?lyrs=s,m&gl=&x={x}&y={y}&z={z}&src=app&scale=2&from=app'
-  
-  });''
-  
+    url: 'https://gac-geo.googlecnapps.cn/maps/vt?lyrs=s,m&hl=&x={x}&y={y}&z={z}&src=app&scale=2&from=app'
+  });
+  //var terrainProvider = new Cesium.ArcGISTiledElevationTerrainProvider({
+    //url: 'https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer'
+//});
+    //viewer.scene.terrainProvider = terrainProvider;
+
+
 /*var title = new Cesium.UrlTemplateImageryProvider({
     //url: 'https://tiles1.geovisearth.com/base/v1/cia/{z}/{x}/{y}?format=png&tmsIds=w&token=82455ef06c72bb3a35bbb4d7d05fd9eceb96a94dc942a056b8feb0e5928ed96f',
      url: 'https://gac-geo.googlecnapps.cn/maps/vt?lyrs=h,m&gl=&x={x}&y={y}&z={z}&src=app&scale=2&from=app',
@@ -61,8 +64,91 @@ var gog = new Cesium.UrlTemplateImageryProvider({
    
    
    viewer.camera.flyTo({
-     destination: Cesium.Cartesian3.fromDegrees(106.8599, 25.6488, 16000000)
+     destination: Cesium.Cartesian3.fromDegrees(106.85332775, 25.65553097,18000000)
    });
-   // //自定义函数名
-   var gog = viewer.imageryLayers.addImageryProvider(gog);
+   //自定义函数名
+   viewer.imageryLayers.addImageryProvider(gog);
    //var title = viewer.imageryLayers.addImageryProvider(title);
+     
+//经纬度
+var jing = document.querySelector('.jing');
+var wei = document.querySelector('.wei');
+viewer.scene.postRender.addEventListener(function() {
+    var ellipsoid = viewer.scene.globe.ellipsoid;
+    var cartesian = viewer.camera.positionWC;
+    var cartographic = ellipsoid.cartesianToCartographic(cartesian);
+    var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(8);
+    var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(8);
+    jing.innerHTML = '经度: ' + longitudeString;
+    wei.innerHTML = '纬度: ' + latitudeString;
+});  
+
+//指南针
+var compassContainer = document.getElementById('compass');
+// 设置初始指南针样式和事件监听器
+updateCompass();
+compassContainer.addEventListener('click', resetCameraPosition);
+
+viewer.scene.postRender.addEventListener(updateCompass);
+
+function updateCompass() {
+    var cameraHeading = viewer.camera.heading;
+    compassContainer.style.transform = 'rotate(' + (-cameraHeading) + 'rad)';
+}
+
+function resetCameraPosition() {
+    var currentHeading = viewer.camera.heading;
+      // 添加延时动画效果
+  setTimeout(function () {
+    viewer.camera.flyTo({
+      destination: viewer.camera.position,
+    
+        orientation: {
+            heading: Cesium.Math.toRadians(0), // 旋转回北方
+            pitch: viewer.camera.pitch,
+            roll: viewer.camera.roll
+          },
+        duration: 1.5
+      });
+    }, 200);
+}
+//定位
+
+navigator.geolocation.getCurrentPosition(
+  function(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    // 创建用户位置的实体
+    var entity = viewer.entities.add({
+      position: Cesium.Cartesian3.fromDegrees(longitude, latitude),
+      billboard: {
+        image: 'img/gaxz.jpg', // 添加图片
+        scale: 0.02, // 图片大小
+
+      },
+      label : {
+        showBackground : true,
+        font : '14px sans-serif',
+        horizontalOrigin : Cesium.HorizontalOrigin.CENTER,
+        verticalOrigin : Cesium.VerticalOrigin.BOTTOM,
+        pixelOffset : new Cesium.Cartesian2(0, -50)  // 文字偏移量
+      }
+    });
+
+    // 添加动画显示到当前位置
+    viewer.camera.flyTo({
+      destination: Cesium.Cartesian3.fromDegrees(longitude, latitude, 1000), // 设置飞行的高度
+      orientation: {
+        heading : Cesium.Math.toRadians(0.0), // 方向
+        pitch : Cesium.Math.toRadians(-90.0),   // 俯仰角
+        roll : 0.0                             // 翻滚角
+      },
+      duration: 3 // 飞行持续时间
+    });
+  },
+  function(error) {
+    //console.error("Error getting the user's location", error);
+  },
+  { enableHighAccuracy: true } // 提高精确度
+);
